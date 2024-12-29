ee8f27be46ab4f6e7f37d6eee9ba5335279b9f2b
tainuth1 <tainuth1@gmail.com>
2024-12-29 13:43:20 +0700

    ListRendering

--- src/components/lessons/ListRendering.vue
+++ src/components/lessons/ListRendering.vue
@@ -1,14 +1,16 @@
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 const myArray = reactive([10, 20, 30, 40, 50]);
 const arrayObj = reactive([
   { name: "Tai Nuth", gender: "Male" },
   { name: "Phya Phya", gender: "Female" },
 ]);
 const nestedArray = reactive([10, 20, [11, 22, 33, 44, 55], 40]);
+const arrayChange = ref([10, 20, 30, 40, 50, 60, 70]);
+arrayChange.value = arrayChange.value.filter((item) => item % 2 == 0);
 </script>
 <template>
-  <div class="w-full grid grid-cols-5">
+  <div class="w-full grid grid-cols-5 gap-y-5">
     <ul class="block">
       <li v-for="arr in myArray" :key="arr" class="text-blue-600">{{ arr }}</li>
     </ul>
@@ -34,8 +36,13 @@ const nestedArray = reactive([10, 20, [11, 22, 33, 44, 55], 40]);
       </li>
     </ul>
     <!-- v-for with rage -->
-    <ul class="mt-5">
+    <ul class="">
       <li v-for="n in 6" :key="n">{{ n }}</li>
+    </ul>
+
+    <!-- Array change detection -->
+    <ul>
+      <li v-for="(arr, index) in arrayChange" :key="index">{{ arr }}</li>
     </ul>
   </div>
 </template>
